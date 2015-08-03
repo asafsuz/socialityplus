d290105370db751dbcbec14a8981a86bd2f0e9d2
maurisadan <mauri2design@gmail.com>
2015-08-03 19:35:17 +0300

    A test with the post query
    the rows 12 trhough 17 are a test I made, successfuly, with two dummy
    parameters. You can erase this rows and return the rows 19 through 22
    and put them instead, without the observation signs.

--- core/Posts.class.php
+++ core/Posts.class.php
@@ -9,11 +9,18 @@ class Posts {
 	}
 
 	public function createNewPost($details ) {
+//a test for the query		
 		$result = $this->_db->query(
+				"INSERT INTO posts( user_id, post_content,post_created )
+			VALUES
+			(202,'This is my new message',NOW())"
+		);
+		
+		/*$result = $this->_db->query(
 			"INSERT INTO posts( user_id, post_content,post_created )
 			VALUES
 			('".$details['user_id']."','".$details['post_content']. "',NOW())"
-		);
+		);*/
 		
 		return $result;
 	}
