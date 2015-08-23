45a905dbb4b4f047a511a56d12d8ec09074dc11c
maurisadan <mauri2design@gmail.com>
2015-08-23 23:33:17 +0300

    cosmetica

--- inc/template_pageTop.php
+++ inc/template_pageTop.php
@@ -1,14 +1,14 @@
 <!-- The template_pageTop.php is the header of the index.php page and includes the logo and the nav tag for the menu -->
 <header>
-		<div id="pageTopInner">
-			<div class="Logo">
-				<a href=""><img src="images/logo.png" alt="Socialityplus Logo" title="Social Network"></a>
-			</div>
-			<nav>
-				<ul>
-					<li>Hello Visitor</li>
-					<li><img src="images/register.png" alt="login register to Socialityplus Logo" title="login register"></li>
-				</ul>
-			</nav>
+	<div id="pageTopInner">
+		<div class="Logo">
+			<a href=""><img src="images/logo.png" alt="Socialityplus Logo" title="Social Network"></a>
 		</div>
+		<nav>
+			<ul>
+				<li>Hello Visitor</li>
+				<li><img src="images/register.png" alt="login register to Socialityplus Logo" title="login register"></li>
+			</ul>
+		</nav>
+	</div>
 </header>
